997683d05a9305d4a0feca0302971b0577470758
Dar3cz3Q <dho1@tlen.pl>
2019-05-16 19:15:07 +0200

    Try again

--- js/main.js
+++ js/main.js
@@ -102,6 +102,6 @@ function ChangeContent() {
     document.getElementsByClassName("SingleContentElement")[system_unique].classList.remove("HiddenElement");
     document.title = number_define[system_unique];
     var url = location.href;
-    url.replaceWith("#", "");
+    url.replace('#', "?");
     $(window).scrollTop(0);
 }
